d15bd54ebee51e48000c7206db0547bc7988baa1
DanaBaradie <baradiedana@gmail.com>
2024-06-07 09:29:59 +0300

    change

--- Task5/Main.java
+++ Task5/Main.java
@@ -21,7 +21,7 @@ public class Main {
             int choice = scanner.nextInt();
 
             switch (choice) {
-                case 1:
+                case 1: 
                     system.listCourses();
                     break;
                 case 2:
